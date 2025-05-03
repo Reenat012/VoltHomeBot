349e2bf599e7a8001db475cb7bd1ddc1214350dd
reenat <reenat056@yandex.ru>
2025-05-03 16:45:20 +0300

    commit 3.05.25

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Конфигурация вебхука
-WEBHOOK_HOST = 'https://ваш_домен.ru'  # Замените на ваш домен
+WEBHOOK_HOST = 'https://83.217.220.42' #Замените на ваш домен
 WEBHOOK_PATH = '/webhook'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
